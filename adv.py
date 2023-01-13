b5b2e751b9d76f6418b83a5fc6f8c2f3c1d4dd45
出久 <118390636+asim1909@users.noreply.github.com>
2023-01-14 03:24:55 +0530

    Create adv.py

---
+++ adv.py
@@ -0,0 +1,10 @@
+# Python program to calculate how many days person survived in this world
+from datetime import date, datetime
+
+def days_lived(birth_date):
+    today = date.today()
+    birth_date = datetime.strptime(birth_date, "%Y-%m-%d").date()
+    return (today - birth_date).days
+
+birth_date = input("Enter your birthdate in YYYY-MM-DD format: ")
+print("You have lived for ", days_lived(birth_date), " days.")
